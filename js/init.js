969ef6d225c7800554cb29c458865972af9ff23a
Wuiszho <luigi040220@gmail.com>
2023-06-21 15:13:29 -0600

    Slider inicial, formato a la página principal

--- js/init.js
+++ js/init.js
@@ -2,6 +2,10 @@
   $(function(){
 
     $('.sidenav').sidenav();
+
+    $(document).ready(function(){
+      $('.slider').slider();
+    });
     
   }); // end of document ready
 })(jQuery); // end of jQuery name space
